c0a53ece1cd8a3b5138af87d46e74352404c7b70
ryan-callahan-edo <ryan.callahan@edoinc.com>
2025-02-11 21:09:03 -0600

    remove some extra stuff

--- lib/auth/query.ts
+++ lib/auth/query.ts
@@ -1,21 +1,7 @@
-import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
-import type { UseSuspenseQueryResult } from "@tanstack/react-query";
+import { queryOptions } from "@tanstack/react-query";
 import { getAuth } from "./api";
-import type { Auth } from "./auth";
 
 export const authQueryOptions = queryOptions({
   queryKey: ["getAuth"],
   queryFn: () => getAuth(),
 });
-
-export const useAuthQuery = () => useSuspenseQuery(authQueryOptions);
-
-export const useAuthedQuery = () => {
-  const authQuery = useAuthQuery();
-
-  if (authQuery.data.isAuthenticated === false) {
-    throw new Error("Not authenticated");
-  }
-
-  return authQuery as UseSuspenseQueryResult<Auth>;
-};
